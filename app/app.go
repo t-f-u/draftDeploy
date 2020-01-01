9da088455ccce60d53573833cffe3e11d012ee37
Nagao Takashi <nagao@eda-inc.jp>
2020-01-02 00:38:06 +0900

    メインメソッドを削除

--- app/app.go
+++ app/app.go
@@ -4,8 +4,6 @@ import (
 	. "draftDeploy/app/convert"
 	. "draftDeploy/app/read"
 	. "draftDeploy/app/write"
-	"log"
-	"os"
 )
 
 func Start(dir string, dist string) error {
@@ -14,14 +12,4 @@ func Start(dir string, dist string) error {
 		return err
 	}
 	return Write(Convert(Read(args)))
-}
-
-func main() {
-	if len(os.Args) != 3 {
-		log.Fatal("there must be 2 arguments: target and distribution")
-	}
-
-	if err := Start(os.Args[1], os.Args[2]); err != nil {
-		log.Fatal(err)
-	}
 }
